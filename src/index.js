afd2b645e402c1069e1bcc574152ec661e0ff06a
dvargas42 <dvargas.eng@gmail.com>
2021-01-24 10:37:13 -0300

    Correction name array.

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.put('/projects/:id', (request, response) => {
 app.delete('/projects/:id', (request, response) => {
     const { id } = request.params;
 
-    const projectIndex = project.findIndex( project => project.id === id );
+    const projectIndex = projects.findIndex( project => project.id === id );
 
     if (projectIndex < 0) {
         return response.status(400).json({ error: 'Project not found!' });
