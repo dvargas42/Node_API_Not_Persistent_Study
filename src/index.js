a400f7c494b604942e4257d830968d2cdeaf7640
dvargas42 <dvargas.eng@gmail.com>
2021-01-23 23:53:19 -0300

    Creating basic routes.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get();
+
+app.post();
+
+app.put();
+
+app.delete();
+
+app.listen(3333, () => {
+    console.log('🚀 Back-end started!')
+})
