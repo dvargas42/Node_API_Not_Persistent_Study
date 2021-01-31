59ac9f59f286acfb60eb456a16b3b1576bacf435
dvargas42 <dvargas.eng@gmail.com>
2021-01-31 10:22:48 -0300

    Middleware logRequest creation.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 const projects = [];
 
+function logRequest(request, response, next){
+    const { method, url } =  request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+
+    return next();
+}
+
+app.use(logRequest);
+
 app.get('/projects', (request, response) => {
     const { title } = request.query;
 
